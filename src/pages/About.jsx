9bc906f536f463f0e5d13e544c92687054e2e30d
Besghaier <besghaier.mohamed.04@gmail.com>
2026-03-26 08:18:40 +0100

    add home image

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,12 @@ function About() {
     <div className="page">
       <title> About </title>
       <h2> About Me </h2>
-      <p> Student, Hacker, Content creator </p>
+      <p>
+        I’m Mohamed, a computer science student with a strong interest in web security and penetration testing.
+        I focus on understanding how web applications work internally and how vulnerabilities can be exploited in real-world scenarios. My learning approach is hands-on, using platforms like labs, CTFs, and personal experiments.
+        I’m currently developing my skills in areas such as web vulnerabilities, reconnaissance, and basic exploitation techniques.
+        I’m looking to grow in cybersecurity and gain practical experience through projects, challenges, and real-world opportunities. 
+      </p>
     </div>
   );
 }
--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,8 +1,17 @@
+import profile from '/src/assets/profile.jpeg'
+
 function Hero() {
   return (
     <div className="page">
       <title> Home </title>
-      <h1> Mohamed Besghaier </h1>
+      <img id='profile' src={profile} alt="profile logo" />
+      <div id='hero-sec'>
+        <h1> Aspiring Web Penetration Tester </h1>
+        <h3> 
+          I identify and exploit vulnerabilities in web applications. <br/>
+          Computer science student focused on cybersecurity and hands-on practice. 
+        </h3>
+      </div>
     </div>
   );
 }
